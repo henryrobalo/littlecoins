ff3b919449cc8a974c036c6ff7aa9b8e428204d1
Henry Robalo <henryrobalo@sapo.pt>
2016-06-22 18:23:00 +0100

    accept and verifies all numbers

--- src/org/academiadecodigo/littlecoins/server/ServerWorker.java
+++ src/org/academiadecodigo/littlecoins/server/ServerWorker.java
@@ -24,7 +24,6 @@ public class ServerWorker implements Runnable {
     private String name;
     private int bet;
     private int guess;
-    private String tName;
 
 
     public ServerWorker(Socket playerSocket, Server server) {
@@ -53,58 +52,28 @@ public class ServerWorker implements Runnable {
 
             out.println("Please enter your name.");
 
+            acceptName();
 
-            while (!correctName) {
-                tName = in.readLine();
-
-                if (server.containsName(tName) || tName.equals("")) {
-
-                    out.println("This name is already in the list. Please change name");
-                    // tName = in.readLine();
-
-
-                } else {
-                    setName(tName);
-                    //out.println("Name changed to " + tName);
-                    correctName = true;
-                    out.println("Your name has changed!");
-                }
-
-
-            }
 
             out.println("You have 3 Coins , choose how many coins you want to bet..");
 
 
             while (!alreadyWin) {
 
-                while (!correctBet) {
 
-                    String tempBet = in.readLine();
+                acceptBet();
 
-                    int value = Integer.parseInt(tempBet);
+                out.println("Please insert your guess between 0 and " + (server.getCounterPlayers() * 3) + " coins\"!!!");
 
-
-                    if (value >= 0 && value <= 3) {
-                        bet = value;
-                        out.println(bet);
-                        System.out.println("Player " + name + " bet is: " + value);
-
-                    } else {
-                        out.println("Make sure you bet between 0 or 3 coins!");
-                        out.println("Place your bet , between 0 - 3 : ");
-                    }
+                acceptGuess();
 
 
-                }
-
-                while (!correctGuess) {
-
+                alreadyWin = false;
+                // playerSocket.close()
 
 
-
-
-
+                while (true) {
+                    String line = in.readLine();
 
                 }
 
@@ -112,14 +81,106 @@ public class ServerWorker implements Runnable {
             }
 
 
-            alreadyWin = false;
-            // playerSocket.close()
+
+
+
+
 
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+
+    }
+
+    private synchronized void acceptName(){
+
+        while (!correctName) {
+            String tName = null;
+            try {
+                tName = in.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (server.containsName(tName) || tName.equals("")) {
+
+                out.println("This name is already in the list. Please change name");
+
+            } else {
+                setName(tName);
+                //out.println("Name changed to " + tName);
+                correctName = true;
+                out.println("Your name was accepted!");
+            }
+
+
+        }
+
+    }
+
+    private  synchronized void acceptBet(){
+
+        while (!correctBet) {
+
+            String tempBet = null;
+            try {
+                tempBet = in.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            int value = Integer.parseInt(tempBet);
+
+
+            if (value >= 0 && value <= 3) {
+                bet = value;
+                out.println(bet);
+                out.println("Bet accept!");//nao alterar este print é a condiçao de saida do while na playerThread
+                System.out.println("Player " + name + " bet is: " + value);
+                correctBet = true;
+
+            } else {
+                out.println("Make sure you bet between 0 or 3 coins!");
+                out.println("Place your bet , between 0 - 3 : ");
+            }
+
+        }
+
+    }
+
+
+    private synchronized void acceptGuess(){
+
+        while (!correctGuess) {
+
+            String temGuess = null;
+            try {
+                temGuess = in.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            int value = Integer.parseInt(temGuess);
+
+
+            if (server.containsGuess(value)) {
+                out.println("The guess " + value + " is already on the list");
+            } else if (value > (server.getCounterPlayers() * 3)) {
+                out.println("Make sure you bet between 0 and " + (server.getCounterPlayers() * 3) + " coins");
+
+            } else {
+                guess = value;
+                out.println(guess);
+                out.println("Guess accept!");//nao alterar este print é a condiçao de saida do while na playerThread
+                System.out.println("Player " + name + " guess is: " + value);
+                correctGuess = true;
+
+            }
+
+
+        }
 
     }
 
